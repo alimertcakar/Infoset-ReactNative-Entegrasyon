c2ac86774acadb78189d8929560f22a6aba81995
Ali Mert Çakar <mail@alimert.net>
2021-06-11 14:59:54 +0300

    code

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
       <View style={{ flex: 1, border: "1px solid red" }}>
         <ChatWidget
           isVisible={true}
-          apiKey="QJd96e0cYwppDjNLRlrCaxQKh0rRB1gK7XVg8xgQ"
+          apiKey="mIpz5fM2zvlSQP0zRihu2UJdRjPB0cFGvRUdnW43"
           iosKey="4PNh0ijgG1HFaBb5Jtof4nAdPB5kcDS1"
           androidKey="7pTD3Cem3BKbQnxRpxFilQNqLfla4kf9"
         />
