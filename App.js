8c8cb44430d0c01f5efccdac84569b568c32af8e
Ali Mert Çakar <mail@alimert.net>
2021-08-05 14:10:50 +0300

    code

--- App.js
+++ App.js
@@ -18,25 +18,24 @@ const visitor = {
 
 export default function App() {
   return (
-    <View>
-      <View style={{ flex: 1, border: "1px solid red" }}>
-        <Text>infoset chat widget</Text>
-        <ChatWidget
-          isVisible={true}
-          apiKey="mIpz5fM2zvlSQP0zRihu2UJdRjPB0cFGvRUdnW43"
-          iosKey="VdWWpj2JbLjfScwOQM0uBmOtU85eOXrv"
-          androidKey="HolFCnNcHivTahSyUluFW2UBqArrCyry"
-          color="#E69618"
-          onWidgetHide={() => console.log("false")}
-          onWidgetWillHide={() => console.log("will hide")}
-          onWidgetWillShow={() => console.log("will show")}
-          onWidgetShow={() => console.log("on show")}
-          onNewMessage={() => console.log("on new message")}
-          handleUrl={(url) => console.log(url)}
-          visitor={visitor}
-          tags={["Destek"]}
-        />
-      </View>
+    <View style={{ flex: 1 }}>
+      <Text>selam</Text>
+
+      <ChatWidget
+        isVisible={true}
+        apiKey="mIpz5fM2zvlSQP0zRihu2UJdRjPB0cFGvRUdnW43"
+        iosKey="VdWWpj2JbLjfScwOQM0uBmOtU85eOXrv"
+        androidKey="HolFCnNcHivTahSyUluFW2UBqArrCyry"
+        color="#E69618"
+        onWidgetHide={() => console.log("false")}
+        onWidgetWillHide={() => console.log("will hide")}
+        onWidgetWillShow={() => null}
+        onWidgetShow={() => null}
+        onNewMessage={() => console.log("on new message")}
+        handleUrl={(url) => console.log(url)}
+        visitor={visitor}
+        tags={["Destek"]}
+      />
     </View>
   );
 }
