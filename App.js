099260e33a45e859b590d42f4a0fbb89897e6483
Ali Mert Çakar <mail@alimert.net>
2021-08-05 09:23:57 +0300

    code

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import {
   ChatWidget,
-  VisitorType,
-  // ChatWidgetProps,
-} from '@infoset/react-native';
+} from "@infoset/react-native";
 
-const visitor: VisitorType = {
+const visitor = {
   id: 123,
-  firstName: 'John',
-  lastName: 'Wick',
-  company: 'infoset',
-  email: 'example@infoset.app',
-  phone: '+901234567890',
-  photoUrl: '',
-  createdAt: '',
-  userHash: '',
+  firstName: "John",
+  lastName: "Wick",
+  company: "infoset",
+  email: "examplee@infoset.app",
+  phone: "+90123456790",
+  photoUrl: "",
+  createdAt: "",
+  userHash: "",
 };
 
 export default function App() {
   return (
-    <>
+    <View>
       <View style={{ flex: 1, border: "1px solid red" }}>
         <Text>infoset chat widget</Text>
         <ChatWidget
@@ -29,17 +27,16 @@ export default function App() {
           iosKey="VdWWpj2JbLjfScwOQM0uBmOtU85eOXrv"
           androidKey="HolFCnNcHivTahSyUluFW2UBqArrCyry"
           color="#E69618"
-          onWidgetHide={() => setVisible(false)}
-          onWidgetWillHide={() => console.log('will hide')}
-          onWidgetWillShow={() => console.log('will show')}
-          onWidgetShow={() => console.log('on show')}
-          onNewMessage={() => console.log('on new message')}
+          onWidgetHide={() => console.log("false")}
+          onWidgetWillHide={() => console.log("will hide")}
+          onWidgetWillShow={() => console.log("will show")}
+          onWidgetShow={() => console.log("on show")}
+          onNewMessage={() => console.log("on new message")}
           handleUrl={(url) => console.log(url)}
           visitor={visitor}
-          tags={['Support', 'Recurring Customer']}
+          tags={["Destek"]}
         />
       </View>
-    </>
+    </View>
   );
 }
-
